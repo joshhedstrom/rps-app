8f8af3c76b44d9071491064000848d15781df958
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-03-23 10:37:06 -0500

    made gameloop functional

--- assets/app.js
+++ assets/app.js
@@ -4,6 +4,12 @@
 let userOneChoice;
 let userTwoChoice;
 
+let onePick;
+let twoPick;
+
+let userOnePicked = false;
+let userTwoPicked = false;
+
 let oneWins = 0;
 let oneLosses = 0;
 let twoWins = 0;
@@ -17,27 +23,29 @@ let scissors;
 let gameCounter = 0;
 
 
+
+
 function gameLoop(userOneChoice, userTwoChoice) {
 
     if (userOneChoice === userTwoChoice) {
         tieGame();
 
-    } else if (userOneChoice === rock || userTwoChoice === scissors) {
+    } else if (userOneChoice === "rock" && userTwoChoice === "scissors") {
         userOneWins();
 
-    } else if (userOneChoice === rock || userTwoChoice === paper) {
+    } else if (userOneChoice === "rock" && userTwoChoice === "paper") {
         userTwoWins();
 
-    } else if (userOneChoice === paper || userTwoChoice === rock) {
+    } else if (userOneChoice === "paper" && userTwoChoice === "rock") {
         userOneWins();
 
-    } else if (userOneChoice === paper || userTwoChoice === scissors) {
+    } else if (userOneChoice === "paper" && userTwoChoice === "scissors") {
         userTwoWins();
 
-    } else if (userOneChoice === scissors || userTwoChoice === rock) {
+    } else if (userOneChoice === "scissors" && userTwoChoice === "rock") {
         userTwoWins();
 
-    } else if (userOneChoice === scissors || userTwoChoice === paper) {
+    } else if (userOneChoice === "scissors" && userTwoChoice === "paper") {
         userOneWins();
 
     }
@@ -50,6 +58,9 @@ function userOneWins() {
     twoLosses++;
     gameCounter++;
     resetGame();
+    updateStats();
+    userOnePicked = false;
+    userTwoPicked = false;
 
 }
 
@@ -59,6 +70,9 @@ function userTwoWins() {
     oneLosses++;
     gameCounter++;
     resetGame();
+    updateStats();
+    userOnePicked = false;
+    userTwoPicked = false;
 
 }
 
@@ -66,6 +80,9 @@ function tieGame() {
     $('#message').text("Tie Game!")
     ties++;
     resetGame();
+    updateStats();
+    userOnePicked = false;
+    userTwoPicked = false;
 
 }
 
@@ -73,6 +90,53 @@ function resetGame() {
 
 }
 
+function updateStats(scoreOne, scoreTwo, ties) {
+    //session update user 1 wins/losses
+    //session update user 2 wins/losses
+    //master update user 1 wins/losses
+    //master update user 2 wins/losses
+    //update if tie 
+}
+
+$('#oneRock').click(function() {
+    onePick = "rock";
+    userOnePicked = true;
+    check();
+});
+$('#onePaper').click(function() {
+    onePick = "paper";
+    userOnePicked = true;
+    check();
+});
+$('#oneScissors').click(function() {
+    onePick = "scissors";
+    userOnePicked = true;
+    check();
+});
+$('#twoRock').click(function() {
+    twoPick = "rock";
+    userTwoPicked = true;
+    check();
+});
+$('#twoPaper').click(function() {
+    twoPick = "paper";
+    userTwoPicked = true;
+    check();
+});
+$('#twoScissors').click(function() {
+    twoPick = "scissors";
+    userTwoPicked = true;
+    check();
+});
+
+function check() {
+    if (userOnePicked && userTwoPicked) {
+        gameLoop(onePick, twoPick);
+    }
+    else {
+        $('#message').text("Waiting for the other player's selection...")
+    }
+}
 
 
-// });
+    // });
